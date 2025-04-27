0baba1793fad5539b3410789fe5ee0c639c0ba33
Marwane40Nadi <marwanenasrietude@gmail.com>
2025-04-28 00:45:39 +0100

    Delete src directory

--- src/img.js
+++
@@ -1,9 +0,0 @@
-import "./Img.css";
-
-export default function Img() {
-  return (
-    <div className="img-container">
-      <img src="/images/how.jpg" alt="كيف تعمل منصتنا؟" className="responsive-img" />
-    </div>
-  );
-}
--- src/services.js
+++
@@ -1,40 +0,0 @@
-import "./services.css";
-const services = [
-  { icon: "🌍", title: "الاستيراد" },
-  { icon: "🌍", title: "التخليص الجمركي" },
-  { icon: "🌍", title: "التخزين والإيفاء بالطلبات" },
-  { icon: "🌍", title: "الشحن" },
-  { icon: "🌍", title: "مركز الاتصال" },
-  { icon: "🌍", title: "تحويل الأموال عند تحصيلها" },
-];
-export default function Services() {
-  return (
-    <>
-      <div id="serDiv">
-        <section id="hero">
-          <span id="tag">خدماتنا</span>
-          <h1 id="hero-title">
-            ركز فقط في طريقة التسويق ودع IBRAND تتولى المهام الصعبة
-          </h1>
-          <p id="hero-text">
-            سواء كنت مسوق إلكتروني، تاجر أو بائع بالجملة، يمكنك الاعتماد على
-            منصة IBRAND في الولوج إلى عالم التجارة الإلكترونية والتسويق لمنتجاتك
-            في دول الشرق الأوسط وشمال إفريقيا ودول أخرى بأقل التكاليف.
-          </p>
-        </section>
-        <div className="service-container">
-          <div className="service-grid">
-            {services.map((service, index) => (
-              <div key={index} className="service-item">
-                <span className="service-icon">{service.icon}</span>
-                <span className="service-title">{service.title}</span>
-                <span className="service-arrow">←</span>
-              </div>
-            ))}
-          </div>
-          <p className="service-text">هل ترغب في الانضمام إلينا؟ <span className="الآن">ابدأ الآن</span></p>
-        </div>
-      </div>
-    </>
-  );
-}
